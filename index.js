111c60aa04b2acef91b5d9c6c5db46301a58be1c
rajamanohar931 <rajamanohar931@gmail.com>
2023-03-27 13:25:00 +0530

    “first”

---
+++ index.js
@@ -0,0 +1,11 @@
+let fName,Email,Phn,call;
+function submitall(){
+fName=document.getElementsByName("fname").value;
+Email=document.getElementsByName("email").value;
+Phn=document.getElementsByName("phn").value;
+call=document.getElementsByName("call").value;
+console.log(fName);
+console.log(Email);
+console.log(Phn);
+console.log(call);
+}
